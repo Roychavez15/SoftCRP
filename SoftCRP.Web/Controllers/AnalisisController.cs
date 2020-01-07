a35f3c483e3e8584dac45e5bccbb119f12327ec7
ROY <rchavez@partners.com.ec>
2020-01-07 18:07:01 -0500

    fix image email

--- SoftCRP.Web/Controllers/AnalisisController.cs
+++ SoftCRP.Web/Controllers/AnalisisController.cs
@@ -281,7 +281,8 @@ namespace SoftCRP.Web.Controllers
                 _mailHelper.SendMailAttachment(emails, "Plataforma Clientes",
                     $"<html xmlns='http://www.w3.org/1999/xhtml'>" +
                     $"<head>" +
-                    $"<meta http-equiv=" + "Content-Type" + " content=" + "text/html; charset = UTF-8" + " />" +
+                    $"<meta http-equiv='Content-Type' content='text/html; charset = UTF-8'/>" +
+                    $"<meta content = 'text/html; charset=utf-8'/>"+
                     $"<title>" +
                     $"</title>" +
                     $"</head>" +
